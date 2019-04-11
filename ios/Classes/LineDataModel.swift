372f01849fbfda70a13e189766c852adc6c95107
fengqiang-mbp <498460828@qq.com>
2019-04-11 16:34:46 +0800

    iOS添加滑动手势处理

---
+++ ios/Classes/LineDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  LineDataModel.swift
+//  flutter_editable_charts
+//
+//  Created by 奉强 on 2019/4/11.
+//
+
+import Foundation
+
+class LineDataModel {
+    
+    var x: Double
+    
+    var y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+}
--- ios/Classes/LineSetView.swift
+++ ios/Classes/LineSetView.swift
@@ -10,7 +10,9 @@ import Charts
 
 class LineSetView: UIView {
     
-    var chartView: LineChartView = {
+    var panGesture: UIPanGestureRecognizer?
+    
+    lazy var chartView: LineChartView = {
         let chartView = LineChartView()
         chartView.gridBackgroundColor = NSUIColor.white
         chartView.chartDescription?.text = ""
@@ -33,8 +35,52 @@ class LineSetView: UIView {
         chartView.pinchZoomEnabled = false
         chartView.doubleTapToZoomEnabled = false
         
+        let data = LineChartData()
+        data.addDataSet(dataSet)
+        
+        chartView.data = data
+        
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(chartViewPan(_:)))
+        chartView.addGestureRecognizer(panGesture!)
+        
         return chartView
     } ()
+    
+    /// 数据集
+    lazy var dataSet: LineChartDataSet = {
+        var yse1 = [ChartDataEntry]()
+        
+        for x in stride(from: 0, to: 4.9, by: 0.5) {
+            yse1.append(ChartDataEntry(x: Double(x), y: 15 * x))
+        }
+        
+        let dataSet = LineChartDataSet(values: yse1, label: nil)
+        dataSet.highlightEnabled = true // 选中拐点,是否开启高亮效果(显示十字线)
+        dataSet.highlightColor = .clear // 十字线颜色
+        dataSet.drawCirclesEnabled = true // 是否绘制拐点
+        dataSet.cubicIntensity = 0.2 // 曲线弧度
+        dataSet.circleRadius = 3 // 打点大小
+        dataSet.circleColors = [UIColor.green] // 圆圈颜色
+        dataSet.valueColors = [.white] // 数值颜色
+        dataSet.valueFont = UIFont.systemFont(ofSize: 10)
+        dataSet.mode = .cubicBezier // 模式为曲线模式
+        dataSet.drawFilledEnabled = true // 是否填充颜色
+        dataSet.drawValuesEnabled = true
+        dataSet.lineWidth = 1.5 // 线宽
+        
+        dataSet.setColor(UIColor.green)
+        
+        let gradientColors = [
+            UIColor.green.cgColor,
+            UIColor.green.withAlphaComponent(0.7).cgColor,
+            ] as CFArray
+        
+        let gradientRef = CGGradient(colorsSpace: nil, colors: gradientColors, locations: nil)
+        dataSet.fillAlpha = 0.5
+        dataSet.fill = Fill(linearGradient: gradientRef!, angle: 90)
+        
+        return dataSet
+    }()
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -48,6 +94,35 @@ class LineSetView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         chartView.frame = self.bounds
+    }
+    
+    /// 图表滑动
+    @objc func chartViewPan(_ pan: UIPanGestureRecognizer) {
+        let view = pan.view as! LineChartView
+        let value = view.valueForTouchPoint(point: pan.location(in: view), axis: .left)
+        valueChange(to: value)
+    }
+    
+    private func valueChange(to value: CGPoint) {
+        // 获取数据源
+        guard let data = chartView.data?.getDataSetByIndex(0) else {
+            return
+        }
+        
+        // 遍历数据，获取最接近的那个
+        for index in 0 ..< data.entryCount {
+            guard let entry = data.entryForIndex(index) else {
+                continue
+            }
+            
+            if abs(entry.x - Double(value.x)) < 0.1 {
+                entry.y = Double(value.y)
+                break
+            }
+        }
+        
+        dataSet.notifyDataSetChanged()
+        chartView.notifyDataSetChanged()
     }
     
 }
