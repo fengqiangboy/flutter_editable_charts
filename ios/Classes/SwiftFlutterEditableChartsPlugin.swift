ffd0b958dd7728310b389cafd346baf6a07e0d51
fengqiang-mbp <498460828@qq.com>
2019-04-11 10:53:31 +0800

    优化iOS版本代码

--- ios/Classes/SwiftFlutterEditableChartsPlugin.swift
+++ ios/Classes/SwiftFlutterEditableChartsPlugin.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterEditableChartsPlugin: NSObject {
-@objc
+  @objc
   public static func register(with registrar: FlutterPluginRegistrar) {
     registrar.register(FlutterEditableChartsFactory(), withId: "com.timeyaa.com/flutter_editable_charts")
   }
