1423b75e90356c701ff2cb537b7cca0fb5a3e3b0
fengqiang-mbp <498460828@qq.com>
2019-04-12 17:09:35 +0800

    no message

--- ios/Classes/ColorUtil.swift
+++ ios/Classes/ColorUtil.swift
@@ -12,6 +12,6 @@ func ColorFromRGB(rgbValue: Int) -> UIColor {
         red: CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0,
         green: CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0,
         blue: CGFloat(rgbValue & 0x000000FF) / 255.0,
-        alpha: CGFloat((rgbValue & 0xFF000000) >> 24) / 255
+        alpha: CGFloat((UInt64(rgbValue) & UInt64(0xFF000000)) >> 24) / 255
     )
 }
